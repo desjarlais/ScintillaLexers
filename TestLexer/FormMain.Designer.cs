a6e363dc19287188fd88c654babe380b5984b0dc
VPKSoft <vpksoft@vpksoft.net>
2019-05-11 12:46:29 +0300

    Added the test application.

---
+++ TestLexer/FormMain.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace TestLexer
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.scintilla = new ScintillaNET.Scintilla();
+            this.msMain = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.odFile = new System.Windows.Forms.OpenFileDialog();
+            this.mnuTestMarkLoad = new System.Windows.Forms.ToolStripMenuItem();
+            this.msMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // scintilla
+            // 
+            this.scintilla.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scintilla.EdgeColor = System.Drawing.Color.Black;
+            this.scintilla.Location = new System.Drawing.Point(0, 24);
+            this.scintilla.Name = "scintilla";
+            this.scintilla.Size = new System.Drawing.Size(800, 426);
+            this.scintilla.TabIndex = 0;
+            this.scintilla.LocationChanged += new System.EventHandler(this.Scintilla_LocationChanged);
+            this.scintilla.Click += new System.EventHandler(this.Scintilla_LocationChanged);
+            // 
+            // msMain
+            // 
+            this.msMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile});
+            this.msMain.Location = new System.Drawing.Point(0, 0);
+            this.msMain.Name = "msMain";
+            this.msMain.Size = new System.Drawing.Size(800, 24);
+            this.msMain.TabIndex = 1;
+            this.msMain.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuOpen,
+            this.mnuTestMarkLoad});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(37, 20);
+            this.mnuFile.Text = "File";
+            // 
+            // mnuOpen
+            // 
+            this.mnuOpen.Name = "mnuOpen";
+            this.mnuOpen.Size = new System.Drawing.Size(180, 22);
+            this.mnuOpen.Text = "Open";
+            this.mnuOpen.Click += new System.EventHandler(this.mnuOpen_Click);
+            // 
+            // odFile
+            // 
+            this.odFile.Filter = "All files|*.*";
+            // 
+            // mnuTestMarkLoad
+            // 
+            this.mnuTestMarkLoad.Name = "mnuTestMarkLoad";
+            this.mnuTestMarkLoad.Size = new System.Drawing.Size(180, 22);
+            this.mnuTestMarkLoad.Text = "Test mark load";
+            this.mnuTestMarkLoad.Click += new System.EventHandler(this.MnuTestMarkLoad_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.scintilla);
+            this.Controls.Add(this.msMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.msMain;
+            this.Name = "FormMain";
+            this.Text = "A test program for the ScintillaNET lexers © VPKSoft 2019";
+            this.msMain.ResumeLayout(false);
+            this.msMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ScintillaNET.Scintilla scintilla;
+        private System.Windows.Forms.MenuStrip msMain;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuOpen;
+        private System.Windows.Forms.OpenFileDialog odFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuTestMarkLoad;
+    }
+}
+
---
+++ TestLexer/FormMain.cs
@@ -0,0 +1,97 @@
+﻿#region License
+/*
+MIT License
+
+Copyright (c) 2019 Petteri Kautonen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#endregion
+
+using ScintillaNET;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using VPKSoft.ScintillaLexers;
+using VPKSoft.ScintillaLexers.ScintillaNotepadPlusPlus;
+using static VPKSoft.ScintillaLexers.LexerEnumerations;
+
+namespace TestLexer
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            scintilla.Tag = -1; // a null reference will occur otherwise..
+            scintilla.TextChanged += scintilla_TextChanged; // this will adjust the line numbering on the left side..
+        }
+
+        private void mnuOpen_Click(object sender, EventArgs e)
+        {
+            if (odFile.ShowDialog() == DialogResult.OK)
+            {                
+                scintilla.Text = File.ReadAllText(odFile.FileName);
+
+//                string fileName = @"C:\Program Files (x86)\Notepad++\themes\Black board.xml";
+                string fileName = @"C:\Program Files (x86)\Notepad++\stylers.model.xml";
+//                string fileName = @"C:\Program Files (x86)\Notepad++\themes\Hello Kitty.xml";
+
+                ScintillaLexers.CreateLexerFromFile(scintilla, odFile.FileName, fileName);
+//                ScintillaLexers.CreateLexer(scintilla, odFile.FileName);
+            }
+        }
+
+        #region https://github.com/jacobslusser/ScintillaNET/wiki/Displaying-Line-Numbers
+        private void scintilla_TextChanged(object sender, EventArgs e)
+        {
+            // save the time when the document was changed..
+            DateTime timeStamp = DateTime.Now;
+            Scintilla scintilla = (Scintilla)sender;
+
+            int maxLineNumberCharLengthFromTag = (int)scintilla.Tag;
+
+            // Did the number of characters in the line number display change?
+            // i.e. nnn VS nn, or nnnn VS nn, etc...
+            var maxLineNumberCharLength = scintilla.Lines.Count.ToString().Length;
+            if (maxLineNumberCharLength == maxLineNumberCharLengthFromTag)
+                return;
+
+            // Calculate the width required to display the last line number
+            // and include some padding for good measure.
+            const int padding = 2;
+            scintilla.Margins[0].Width = scintilla.TextWidth(Style.LineNumber, new string('9', maxLineNumberCharLength + 1)) + padding;
+            scintilla.Tag = maxLineNumberCharLength;
+        }
+        #endregion
+
+        private void Scintilla_LocationChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show(scintilla.GetStyleAt(scintilla.CurrentPosition).ToString());
+        }
+
+        private void MnuTestMarkLoad_Click(object sender, EventArgs e)
+        {
+            string fileName = @"C:\Program Files (x86)\Notepad++\themes\Black board.xml";
+            var markColors = MarkColors.FromFile(fileName);
+            MessageBox.Show(markColors.SmartHighLightingForeground.ToString());
+        }
+    }
+}
